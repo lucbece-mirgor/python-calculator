51210c5e7617b501ed047c2fa15cd195d3761416
lucbece-mirgor <lucas.becerra@mirgor.com>
2023-11-01 12:10:50 -0300

    Add main routine file

---
+++ main.py
@@ -0,0 +1,25 @@
+from calculadora import Calculator
+
+def main_menu():
+    calculadora = Calculator()
+    calculadora.realizar_operacion()
+
+    while True:
+        print('Ingrese "si" para salir del calculador ')
+        print('Ingrese "historial" para visualizar las operaciones realizadas ')
+        print('Cualquier otro input iniciara un nuevo calculo ')
+        
+        exit = input(': ')
+
+        if exit == 'si':
+            break
+        elif exit == 'historial':
+            calculadora.mostrar_historial()
+            continue
+
+        num1, num2 = calculadora.leer_enteros()
+        calculadora.setear_enteros(num1, num2)
+        calculadora.realizar_operacion()
+    return
+
+main_menu()
